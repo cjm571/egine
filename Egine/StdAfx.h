cef8d57054710223475212568588dacc5a50c897
cjm571 <cjm571@gmail.com>
2014-02-12 14:24:29 -0600

    Working code!

--- Egine/StdAfx.h
+++ Egine/StdAfx.h
@@ -3,6 +3,24 @@
 *                        *
 * Created on: 2014-02-07 *
 *************************/
+#pragma once
+
+// Modify the following defines if you have to target a platform prior to the ones specified below.
+// Refer to MSDN for the latest info on corresponding values for different platforms.
+#ifndef WINVER              // Allow use of features specific to Windows 7 or later.
+#define WINVER 0x0700       // Change this to the appropriate value to target other versions of Windows.
+#endif
+
+#ifndef _WIN32_WINNT        // Allow use of features specific to Windows 7 or later.
+#define _WIN32_WINNT 0x0700 // Change this to the appropriate value to target other versions of Windows.
+#endif
+
+#ifndef UNICODE
+#define UNICODE
+#endif
+
+// Exclude rarely-used items from Windows headers.
+#define WIN32_LEAN_AND_MEAN
 
 // Windows header files
 #include <Windows.h>
